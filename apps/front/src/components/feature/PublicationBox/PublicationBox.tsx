9ece36fe6c59238fe5bb0d2ad7f014f2b0dd3867
Jes015 <jes.18.sin.15@gmail.com>
2024-01-18 22:16:40 -0500

    feat(front): add Publicaiton Box component (#1)
    close #1

--- apps/front/src/components/feature/PublicationBox/PublicationBox.tsx
+++ apps/front/src/components/feature/PublicationBox/PublicationBox.tsx
@@ -1,5 +1,5 @@
 import { BaseComponentType } from "@/models"
-import { Button, TextArea } from "@radix-ui/themes"
+import { Button, TextArea, TextField } from "@radix-ui/themes"
 
 export const PublicationBox: BaseComponentType = ({ children, ...props }) => {
     return (
@@ -11,6 +11,11 @@ export const PublicationBox: BaseComponentType = ({ children, ...props }) => {
             }
             {...props}
         >
+            <TextField.Root>
+                <TextField.Input
+                    placeholder="Photo with bad bunny and arcangel in our last single <3"
+                />
+            </TextField.Root>
             <TextArea placeholder="Reply to comment…" />
             <footer
                 className="flex justify-end" 
